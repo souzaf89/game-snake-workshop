83ba914acbad4049559abaf74bef8af549f97370
Fagner Souza <fagner.roberto89@gmail.com>
2020-11-07 06:41:58 -0300

    Game Snake Workshop, concluded in 4 days following the classes, working goodgit add .!

---
+++ src/br/com/softblue/snake/Application.java
@@ -0,0 +1,13 @@
+package br.com.softblue.snake;
+
+import br.com.softblue.core.Game;
+
+public class Application {
+
+	public static void main(String[] args) {
+		 Game g = new Game();
+		 g.start();
+		
+	}
+
+}
---
+++ src/br/com/softblue/snake/scene/GameOverText.java
@@ -0,0 +1,14 @@
+package br.com.softblue.snake.scene;
+
+import java.awt.Color;
+import java.awt.Point;
+
+import br.com.softblue.snake.graphics.Text;
+import br.com.softblue.snake.util.Constants;
+
+public class GameOverText extends Text {
+
+	public GameOverText (int score) {
+		super(Constants.GAME_OVER_COLOR,  String.format (Constants.GAME_OVER_TEXT, score), Constants.GAME_OVER_LOCATION);
+	}
+}
